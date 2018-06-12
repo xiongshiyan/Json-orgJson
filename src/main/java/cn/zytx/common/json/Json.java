06c01545b49e9ab4437f1a2ea18d4a5bd154803c
xxssyyyyssxx <xxssyyyyssxx@126.com>
2018-06-12 12:53:04 +0800

    兼容处理不放在json接口中

--- src/main/java/cn/zytx/common/json/Json.java
+++ src/main/java/cn/zytx/common/json/Json.java
@@ -37,18 +37,6 @@ public interface Json<T extends Json> {
     T setStrict(boolean isStrict);
 
     /**
-     * 是否类型宽容
-     * @return true if is typed-tolerant
-     */
-    default boolean isTolerant(){return true;}
-
-    /**
-     * 设置是否宽容
-     * @param isTolerant true if is typed-tolerant
-     */
-    T setTolerant(boolean isTolerant);
-
-    /**
      * 具体的实现类
      */
     Object unwrap();
